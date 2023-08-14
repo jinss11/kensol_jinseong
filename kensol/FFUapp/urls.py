3b94d3a6680af21085db804f1da005a622b0d114
jinss11 <umji205@naver.com>
2023-08-14 10:46:06 +0900

    url 수정
    url 수정 pdf 제거

--- kensol/FFUapp/urls.py
+++ kensol/FFUapp/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('FFUInput/', views.ffuInput),      # 'ffuInput/' : 웹페이지 URL에서 ffuInput/ | # views.ffuInput : 앞서 import한 views.py의 ffuInput 함수 
     path('FFUOutput/', views.ffuOutput),
     path('ffuCalculate/', views.ffuCalculate),
-    path('convert_to_pdf/', views.convert_to_pdf, name='convert_to_pdf'),
 ]
