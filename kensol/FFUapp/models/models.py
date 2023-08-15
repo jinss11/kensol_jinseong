b31478dc4fe6f8f6aed7d6fe5e0389476546a222
jinss11 <umji205@naver.com>
2023-08-15 22:42:39 +0900

    db연동 및 model만들기
    postgre sql과 django 연동 및 models.py 만들어보기

--- kensol/FFUapp/models/models.py
+++ kensol/FFUapp/models/models.py
@@ -121,3 +121,160 @@ class Option(models.Model):
                                 related_name = 'option',
                                 on_delete = models.SET_NULL,
                                 null = True)
+    
+
+
+#############################################
+############# ffu 샘플 데이터 ################
+#############################################
+
+## 일반사양 자재비 ##
+class GenMaterialcost(models.Model):
+    item = models.CharField(primary_key=True, max_length=32) # item  # The composite primary key (item, size) found, that is not supported. The first column is selected.
+    size = models.CharField(max_length=16) # ffu 규격
+    matherialsize_width = models.FloatField() # 자재사이즈 가로
+    matherialsize_length = models.FloatField() # 자재사이즈 세로
+    rawmaterial_thickness = models.FloatField() # 원자재 두께
+    rawmaterial_density = models.FloatField() # 원자재 비중
+    manufacture_quantity = models.IntegerField() # 자재 가공수량
+    necessary_quantity = models.IntegerField() # 자재 필요수량
+
+    class Meta:
+        managed = False
+        db_table = 'gen_materialcost'
+        unique_together = (('item', 'size'),)
+
+
+## 고사양 자재비 ##
+class HighMaterialcost(models.Model):
+    item = models.CharField(primary_key=True, max_length=32) # item  # The composite primary key (item, size) found, that is not supported. The first column is selected.
+    size = models.CharField(max_length=16) # ffu 규격
+    matherialsize_width = models.FloatField() # 자재사이즈 가로
+    matherialsize_length = models.FloatField() # 자재사이즈 세로
+    rawmaterial_thickness = models.FloatField() # 원자재 두께
+    rawmaterial_density = models.FloatField() # 원자재 비중
+    manufacture_quantity = models.IntegerField() # 자재 가공수량
+    necessary_quantity = models.IntegerField() # 자재 필요수량
+
+    class Meta:
+        managed = False
+        db_table = 'high_materialcost'
+        unique_together = (('item', 'size'),)
+
+
+## 일반사양 도장비 ##
+class GenPaint(models.Model):
+    item = models.CharField(primary_key=True, max_length=32) # item  # The composite primary key (item, size) found, that is not supported. The first column is selected.
+    size = models.CharField(max_length=16) # ffu 규격
+    figure_width = models.FloatField() # 전개사이즈 가로
+    figure_length = models.FloatField() # 전개사이즈 세로
+    won_per_meter = models.IntegerField() # 원/m^2
+
+    class Meta:
+        managed = False
+        db_table = 'gen_paint'
+        unique_together = (('item', 'size'),)
+
+
+## 고사양 도장비 ##
+class HighPaint(models.Model):
+    item = models.CharField(primary_key=True, max_length=32)  # item  # The composite primary key (item, size) found, that is not supported. The first column is selected.
+    size = models.CharField(max_length=16) # ffu 규격
+    figure_width = models.FloatField() # 전개사이즈 가로
+    figure_length = models.FloatField() # 전개사이즈 세로
+    won_per_meter = models.IntegerField() # 원/m^2
+
+    class Meta:
+        managed = False
+        db_table = 'high_paint'
+        unique_together = (('item', 'size'),)
+
+
+## 일반사양 볼트 ##
+class GenVolt(models.Model):
+    size = models.CharField(primary_key=True, max_length=50) # ffu 규격  # The composite primary key (size, volt_name) found, that is not supported. The first column is selected.
+    volt_name = models.CharField(max_length=50) # 볼트 품명 + 규격
+    volt_price = models.IntegerField() # 단가
+    volt_count = models.IntegerField() # 수량
+
+    class Meta:
+        managed = False
+        db_table = 'gen_volt'
+        unique_together = (('size', 'volt_name'),)
+
+
+## 고사양 볼트 ##
+class HighVolt(models.Model):
+    size = models.CharField(primary_key=True, max_length=50) # ffu 규격  # The composite primary key (size, volt_name) found, that is not supported. The first column is selected.
+    volt_name = models.CharField(max_length=50)  # 볼트 품명 + 규격
+    volt_price = models.IntegerField() # 단가
+    volt_count = models.IntegerField() # 수량
+
+    class Meta:
+        managed = False
+        db_table = 'high_volt'
+        unique_together = (('size', 'volt_name'),)
+
+
+## 일반사양 NCT 가공비 ##
+class GenNct(models.Model):
+    size = models.CharField(primary_key=True, max_length=16) # ffu 규격
+    nct_price = models.FloatField() # nct 판금 가공비
+
+    class Meta:
+        managed = False
+        db_table = 'gen_nct'
+
+
+## 고사양 NCT 가공비 ##
+class HighNct(models.Model):
+    size = models.CharField(primary_key=True, max_length=16) # ffu 규격
+    nct_price = models.FloatField() # nct 판금 가공비 
+
+    class Meta:
+        managed = False
+        db_table = 'high_nct'
+
+
+## 일반사양 조립비 ##
+class GenAssembly(models.Model):
+    size = models.CharField(primary_key=True, max_length=16) # ffu 규격  # The composite primary key (size, assembly_price) found, that is not supported. The first column is selected.
+    assembly_price = models.IntegerField() # 조립 인건비
+
+    class Meta:
+        managed = False
+        db_table = 'gen_assembly'
+        unique_together = (('size', 'assembly_price'),)
+
+
+## 고사양 조립비 ##
+class HighAssembly(models.Model):
+    size = models.CharField(primary_key=True, max_length=16) # ffu 규격  # The composite primary key (size, assembly_price) found, that is not supported. The first column is selected.
+    assembly_price = models.IntegerField() # 조립 인건비
+
+    class Meta:
+        managed = False
+        db_table = 'high_assembly'
+        unique_together = (('size', 'assembly_price'),)
+
+
+## 일반사양 포장비 ##
+class GenPack(models.Model):
+    size = models.CharField(primary_key=True, max_length=16) # ffu 규격  # The composite primary key (size, pack_price) found, that is not supported. The first column is selected.
+    pack_price = models.IntegerField() # 포장비
+
+    class Meta:
+        managed = False
+        db_table = 'gen_pack'
+        unique_together = (('size', 'pack_price'),)
+
+
+## 고사양 포장비 ##
+class HighPack(models.Model):
+    size = models.CharField(primary_key=True, max_length=16) # ffu 규격  # The composite primary key (size, pack_price) found, that is not supported. The first column is selected.
+    pack_price = models.IntegerField() # 포장비
+
+    class Meta:
+        managed = False
+        db_table = 'high_pack'
+        unique_together = (('size', 'pack_price'),)
